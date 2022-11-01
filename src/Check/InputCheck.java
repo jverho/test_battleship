ba7e707a717fcebc51c420dfbc0343337ab4239f
jverho <82749396+jverho@users.noreply.github.com>
2022-11-01 12:57:18 +0100

    - deleted outdated startTurn method - deleted notPLaced method - deleted checkPlacement method

--- src/Check/InputCheck.java
+++ src/Check/InputCheck.java
@@ -23,57 +23,17 @@ public class InputCheck {
             if (!numbers.contains(check_number)){
                 System.out.println("second part of coordinates must contain one os these numbers: 0123456789");
                 return false;
-            } else if (Grid.targetcells.shotGet(coordinate)){
+            }
+            if (Grid.targetcells.shotGet(coordinate)){
                 System.out.printf("the coordinate %s has been hit already\n", coordinate);
                 return false;
             }
             //also add a check for the computer depending on implementation
+            else {return true;}
         }
-        return true;
+
     }
 
-    public static boolean checkPlacement(String from_to, int currentShipLength){
-        if (from_to.length() != 5){
-            System.out.println("entry should contain 5 characters");
-            return false;
-        }
-        else{
-            if (!from_to.contains(",")){
-                System.out.println("entry must contain a , to separate coordinates");
-                return false;
-            }
-            String[] arrofStrPl = from_to.split(",");
-            String firstCoord = arrofStrPl[0];
-            String secondCoord = arrofStrPl[1];
-            char firstCoordLetter = firstCoord.charAt(0);
-            String[] firstCoordList = firstCoord.split("");
-            int firstCoordNumber = Integer.parseInt(firstCoordList[1]);
-            char secondCoordLetter = secondCoord.charAt(0);
-            String[] secondCoordList = secondCoord.split("");
-            int secondCoordNumber = Integer.parseInt(secondCoordList[1]);
-
-            //nothing has been hit, checkShot can be used to check correctness of placement coordinates
-            if (!checkShot(firstCoord)){
-                return false;
-            }
-            if (!checkShot(secondCoord)){
-                return false;
-            }
-            //checks that the ship is not placed diagonally
-            if (firstCoordLetter != secondCoordLetter && firstCoordNumber != secondCoordNumber){
-                System.out.println("Ships can not be placed diagonally");
-                return false;
-            }
-            //checks that the ship has the right length
-            if (Math.abs(firstCoordLetter-secondCoordLetter) != currentShipLength &&
-                    Math.abs(firstCoordNumber-secondCoordNumber) != currentShipLength){
-                System.out.println("Ship does not fit into coordinates entered");
-                return false;
-            }
-            //check if a boat is already on the coordinates
-        }
-        return true;
-    }
 
 
     public static boolean checkPlacementShip(String from_to, Ship currentShip){
--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,6 @@ public class Main {
         //checks if the coordinate has been entered correctly and if the coordinate has been hit
         //if the cell can be shot, it returns true
         new Grid();
-        /*InputCheck.checkShot("A3"); //true
-        Grid.targetcells.shotSet("B6",true);
-        InputCheck.checkShot("B6"); //false
-        Grid.targetcells.boatSet("C1", true);
-        Ship carrier1 = new Ship(ShipType.CARRIER, null);
-        InputCheck.checkPlacementShip("A9,A3", carrier1);
-        InputCheck.checkPlacementShip("A1,F1", carrier1);
-        */
         //starts the game, asks the user to enter coordinates of the ships, does nothing for the computer yet
         //Turn.startTurn();
         Turn.startTurnShips();
